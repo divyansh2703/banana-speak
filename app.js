27c4a972084aef66bb1a1fa895195ffb954e299d
divyansh <doshdivyansh1@gmail.com>
2023-05-30 15:57:52 +0530

    tet area input

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // var username = prompt("give me your name?")
 // alert("this script is working" + username )
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput)
+
+function clickHandler(){
+    console.log("clicked!");
+    console.log("input",txtInput.value);
+    
+};
+btnTranslate.addEventListener("click", clickHandler)
+// console.log(btnTranslate)
